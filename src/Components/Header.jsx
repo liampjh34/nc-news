6196b289ec468209f21d42c16ea463587c1b6d18
Liam Houghton <liamhoughton@192.168.1.198>
2024-06-23 19:42:45 +0100

    finalise header styling

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,17 +6,17 @@ import { Container } from "react-bootstrap"
 export default function Header() {
     return <Navbar 
         collapseOnSelect 
-        expand="lg" 
+        expand="sm" 
         data-bs-theme="light"
         bg="primary"
         id="header">
         <Container>
-            <Navbar.Brand id="navbar-brand" href='/'>big news.</Navbar.Brand>
+            <Navbar.Brand id="navbar-brand" href='/'>big news</Navbar.Brand>
             <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
             <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className='me-auto'>
-                    <Nav.Link href='/articles' className="nav-linked">Articles</Nav.Link>
-                    <Nav.Link href='/topics'>Topics</Nav.Link>
+                <Nav className='ms-auto'>
+                    <Nav.Link href='/articles' className="nav-linked">articles</Nav.Link>
+                    <Nav.Link href='/topics'>topics</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Container>
